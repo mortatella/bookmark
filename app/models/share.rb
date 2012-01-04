03b7a24dbc70302ea380a0963bb5fd813a2ec313
Franz.Brandstaetter <fbrandstaetter.mmt-m2011@fh-salzburg.ac.at>
2012-01-04 15:54:28 +0100

    share validation

--- app/models/share.rb
+++ app/models/share.rb
@@ -2,6 +2,14 @@ class Share < ActiveRecord::Base
   belongs_to :user
   belongs_to :list
   
-  validates :user_id, :presence => { :message => "User(s) not found" }
+  validates :user_id, :presence => { :message => "not found" }
+  validate :validateCurrentUser
+  
+ @list = List.find(params[:id])
+  @share.list = @list
+  
+  def validateCurrentUser
+    errors.add(:user_id, ", can't share list with yourself") if user_id == @share.list.user.id
+  end
   
 end
